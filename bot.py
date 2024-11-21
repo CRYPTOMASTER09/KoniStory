d36bee86082b55c5dc1d1697dff5a279bd11b5c8
CRYPTOMASTER09 <technicalakmal225@gmail.com>
2024-11-21 18:07:34 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -39,10 +39,10 @@ class KoniStory:
     def welcome(self):
         print(
             f"""
-        {Fore.GREEN + Style.BRIGHT}Koni Story Script by {Fore.BLUE + Style.BRIGHT} HACKER WORLD BD
+        {Fore.GREEN + Style.BRIGHT}Koni Story Script by {Fore.BLUE + Style.BRIGHT} CRYPTO MASTER
             """
             f"""
-        {Fore.GREEN + Style.BRIGHT}Join Script Channel {Fore.YELLOW + Style.BRIGHT}https://t.me/HACKER_WORLD_BD
+        {Fore.GREEN + Style.BRIGHT}Join Script Channel {Fore.YELLOW + Style.BRIGHT}https://t.me/airdropconfirm97
             """
         )
 
